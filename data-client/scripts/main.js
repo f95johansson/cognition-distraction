b5813792ca576bfecd0d701b110b588cdd3e98d1
Fredrik Johansson <f95johansson@gmail.com>
2018-01-29 17:38:46 +0100

    Heatmap sho should be working
     - After test is finished a json of the result is downloadable

--- data-client/scripts/main.js
+++ data-client/scripts/main.js
@@ -1,28 +1,41 @@
+function downloadData(id, data, time, withDistraction) {
+  var toDownload = {id: id, 
+                    data: data, 
+                    elapsed_time: time, 
+                    with_distraction: withDistraction};
 
-/*
-var heatmapInstance = h337.create({
-  container: document.querySelector('.heatmap'),
-  radius: 90
-});
-document.querySelector('.heatmap').onmousemove = function(ev) {
-  console.log("Yepp");
-  heatmapInstance.addData({
-    x: ev.layerX,
-    y: ev.layerY,
-    value: 1
-  });
-};
-*/
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(toDownload)));
+  element.setAttribute('download', "experiment_"+id+".json");
 
+  element.style.display = 'none';
+  document.body.appendChild(element);
 
+  element.click();
+
+  document.body.removeChild(element);
+}
 
 
 document.querySelector('#start').onclick = function(e) {
+  fetch("http://localhost:4567/starttimer", {
+    method: "POST",
+  });
+
   document.querySelector('.test').classList.remove('gone');
   document.querySelector('.before').classList.add('gone');
-}
+};
 
 document.querySelector('#stop').onclick = function(e) {
+  fetch("http://localhost:4567/stoptimer", {
+    method: "GET",
+  }).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    console.log(data.id);
+    downloadData(data.id, data.data, data.elapsed_time, false);
+  });
+
   document.querySelector('.test').classList.add('gone');
   document.querySelector('.before').classList.remove('gone');
-}
+};
