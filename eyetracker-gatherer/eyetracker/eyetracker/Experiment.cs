a9470e1dd29956c2b4bfe9751148ea8ae535344c
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2018-01-29 11:15:27 +0100

    Add eye tracker gatherer

---
+++ eyetracker-gatherer/eyetracker/eyetracker/Experiment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace eyetracker
+{
+    class Experiment
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int id { get; set; }
+
+        [JsonProperty(PropertyName = "elapsed_time")]
+        public int elapsed_time { get; set; }
+
+        [JsonProperty(PropertyName = "with_distraction")]
+        public bool with_distraction { get; set; }
+
+        [JsonProperty(PropertyName = "data")]
+        public LinkedList<GazePoint> data;
+
+        public Experiment(int id, int elapsed_time, bool with_distraction, LinkedList<GazePoint> data)
+        {
+            this.id = id;
+            this.elapsed_time = elapsed_time;
+            this.with_distraction = with_distraction;
+            this.data = data;
+        }
+    }
+}
---
+++ eyetracker-gatherer/eyetracker/eyetracker/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Tobii.Interaction;
+using uhttpsharp;
+using uhttpsharp.Handlers;
+using uhttpsharp.Listeners;
+using uhttpsharp.RequestProviders;
+using uhttpsharpdemo;
+using uhttpsharpdemo.Handlers;
+
+namespace eyetracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExperimentRunner.load();
+
+            using (var httpServer = new HttpServer(new HttpRequestProvider()))
+            {
+                httpServer.Use(new TcpListenerAdapter(new TcpListener(IPAddress.Loopback, 1337)));
+
+                httpServer.Use(new HttpRouter().With(string.Empty, new IndexHandler())
+                     .With("start", new StartHandler())
+                     .With("startdistraction", new StartWithDistractionHandler())
+                     .With("stop", new StopHandler())
+                     .With("strings", new RestHandler<string>(new StringsRestController(), JsonResponseProvider.Default)));
+
+                httpServer.Use((context, next) => {
+                    Console.WriteLine("Got Request!");
+                    return next();
+                });
+
+                httpServer.Start();
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
