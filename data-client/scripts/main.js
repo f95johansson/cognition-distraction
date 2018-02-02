6f38b92125cf8f67a8a9a4433e447edbeac9fafd
Fredrik Johansson <f95johansson@gmail.com>
2018-02-02 14:37:45 +0100

    Added real ads with some animations

--- data-client/scripts/main.js
+++ data-client/scripts/main.js
@@ -1,12 +1,30 @@
 
 document.querySelector('#start').onclick = function(e) {
-  fetch("http://localhost:1337/startdistraction", {
+  fetch("http://localhost:1337/start", {
     method: "POST",
-  });
+  }).catch(function(err) {});
 
   document.querySelector('.test').classList.remove('gone');
   document.querySelector('.before').classList.add('gone');
+  var ads = document.getElementsByClassName('ad');
+  for (var i = 0; i < ads.length; i++) {
+    ads[i].classList.add('gone');
+  }
 };
+
+document.querySelector('#startWithDistraction').onclick = function(e) {
+  fetch("http://localhost:1337/startdistraction", {
+    method: "POST",
+  }).catch(function(err) {});
+
+  document.querySelector('.test').classList.remove('gone');
+  document.querySelector('.before').classList.add('gone');
+  var ads = document.getElementsByClassName('ad');
+  for (var i = 0; i < ads.length; i++) {
+    ads[i].classList.remove('gone');
+  }
+};
+
 
 document.querySelector('#stop').onclick = function(e) {
   fetch("http://localhost:1337/stop", {
@@ -16,3 +34,45 @@ document.querySelector('#stop').onclick = function(e) {
   document.querySelector('.test').classList.add('gone');
   document.querySelector('.before').classList.remove('gone');
 };
+
+
+/* Animation Section */
+
+var blocket = document.getElementById('blocket');
+var hygglo = document.getElementById('hygglo');
+var samsung = document.getElementById('samsung');
+var prisjakt = document.getElementById('prisjakt');
+
+var leftFirst = blocket;
+var leftTemp = blocket;
+var leftSecond = samsung;
+
+var rightFirst = hygglo;
+var rightTemp = hygglo;
+var rightSecond = prisjakt;
+
+setInterval(function() {
+  leftFirst.style.opacity = 0;
+  leftTemp = leftFirst;
+  leftFirst = leftSecond;
+
+  setTimeout(function() {
+    leftSecond.style.opacity = 1;
+    leftSecond = leftTemp;
+  }, 1000);
+}, 8000);
+
+
+setTimeout(function() {
+  setInterval(function() {
+    rightFirst.style.opacity = 0;
+    rightTemp = rightFirst;
+    rightFirst = rightSecond;
+
+    setTimeout(function() {
+      rightSecond.style.opacity = 1;
+      rightSecond = rightTemp;
+    }, 1000);
+  }, 8000);
+}, 4000);
+
